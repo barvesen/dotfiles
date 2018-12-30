442954c867778efd80171c7bef89557eda9a82b2
barvesen <29226396+barvesen@users.noreply.github.com>
2018-12-30 19:04:17 +0000

    update vim plug install

--- linux_utils.py
+++ linux_utils.py
@@ -46,7 +46,8 @@ def install_npm_packages(packages):
     subprocess.check_call('npm install -g {}'.format(' '.join(packages['npm_packages'])).split())
 
 def install_vim_plug():
-    subprocess.check_call('curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'.split())
+    # subprocess.check_call('curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'.split())
+    subprocess.check_call('curl -fLo {} --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'.format(os.path.join(os.path.expanduser("~"), '.local', 'share', 'nvim', 'site', 'autoload', 'plug.vim')).split())
 
 def install_rust():
     with tempdir() as dirpath:
